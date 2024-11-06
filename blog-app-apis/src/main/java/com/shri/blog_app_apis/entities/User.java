1dd6579420b74de5d1c3ace30a0a3eb87f0d25c0
Jayshri <jayshreerajput2303gmail.com>
2024-11-06 22:09:02 +0530

    user entity file

---
+++ blog-app-apis/src/main/java/com/shri/blog_app_apis/entities/User.java
@@ -0,0 +1,63 @@
+package com.shri.blog_app_apis.entities;
+
+import org.hibernate.annotations.Collate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int id;
+@Column(name="user_name",nullable=false,length=100)
+private String name;
+@Column(name="user_email")
+private String email;
+@Column(name="user_password")
+private String password;
+@Column(name="user_about")
+private String about;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getAbout() {
+	return about;
+}
+public void setAbout(String about) {
+	this.about = about;
+}
+@Override
+public String toString() {
+	return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", about=" + about
+			+ "]";
+}
+
+
+}
