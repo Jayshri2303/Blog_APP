d688923e4589e8c3423251192f68d10fcb57f08d
Jayshri <jayshreerajput2303gmail.com>
2024-11-08 00:39:04 +0530

    test case for user repo interface

--- blog-app-apis/src/test/java/com/shri/blog_app_apis/BlogAppApisApplicationTests.java
+++ blog-app-apis/src/test/java/com/shri/blog_app_apis/BlogAppApisApplicationTests.java
@@ -3,11 +3,19 @@ package com.shri.blog_app_apis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.shri.blog_app_apis.Repositories.UserRepo;
+
 @SpringBootTest
 class BlogAppApisApplicationTests {
+	private UserRepo userRepo;
 
 	@Test
 	void contextLoads() {
 	}
-
+    @Test
+	public void repoTest()
+	{
+		System.out.println(this.userRepo.getClass().getName());
+		System.out.println(this.userRepo.getClass().getPackageName());
+	}
 }
